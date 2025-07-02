85a46a18b28b9cfe85779bc709764ca2beeac404
Hasegos <csw020106@naver.com>
2025-07-02 10:50:28 +0900

    Test

---
+++ src/test/java/com/example/online_community/OnlineCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
