848c7ed3fe0ef32b7f01bfd1629070028cd93c57
Hasegos <csw020106@naver.com>
2025-07-02 10:58:45 +0900

    Application

---
+++ src/main/java/com/example/online_community/OnlineCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.example.online_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCommunityApplication.class, args);
+	}
+}
